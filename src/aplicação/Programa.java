7568524c478f7e09a5c1243dc3c9d5a0a4e10a30
Claudiomar <claudiomar.junior@aluno.unifametro.edu.br>
2020-02-06 22:52:53 -0300

    leitor de produto em estoque

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -19,9 +19,19 @@ public class Programa {
 		System.out.println("quantidade em estoque: ");
 		prod.quantidade = sc.nextInt();
 
-System.out.println("Dados do produto: " + prod.toString());		
+		System.out.println("Dados do produto: " + prod.toString());
+		System.out.println("Entrada no estoque: "); 
+		int quantidade = sc.nextInt();
+		prod.AdicProduto(quantidade);
 		
+		System.out.println("Dados do produto: " + prod.toString());
+		System.out.println("Saída no estoque: "); 
+		quantidade = sc.nextInt();
+		prod.RemoveProduto(quantidade);
 		
-	sc.close();}
+		System.out.println("Dados do produto: " + prod.toString());
+
+		sc.close();
+	}
 
 }
